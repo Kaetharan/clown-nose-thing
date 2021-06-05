1b3ac881b4a534e677ddbaaaf4a0972583452779
Kaetharan <78970710+Kaetharan@users.noreply.github.com>
2021-06-05 12:58:07 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+noseX=0;
+noseY=0;
+
+function preload(){
+clown_nose=loadImage('https://i.postimg.cc/Cx3KjbhN/Clown-nose-large.png');
+}
+
+function setup(){
+    canvas=createCanvas(300, 300);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(300, 300);    
+    video.hide();
+    poseNet=ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw(){
+image(video, 0, 0, 300, 300);
+image(clown_nose, noseX, noseY, 30, 30);
+}
+
+function takeSnapshot(){
+    save('myfilter.png');
+}
+
+function modelLoaded(){
+    console.log("PoseNet is initializing");
+}
+
+function gotPoses(results){
+if(results.length>0){
+    console.log(results);
+    noseX=results[0].pose.nose.X-25;
+    noseY=results[0].pose.nose.Y-25;
+}
+}
